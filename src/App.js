625cd64393704882c637926dab32ce189e789d79
Matt Slone <slonem01@gmail.com>
2020-06-20 06:50:04 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
+import ProTip from './ProTip';
+import Drawer from './components/Drawer'
+import Gallery from './components/Gallery'
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}));
+
+export default function App() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Drawer />
+      <main className={classes.content}>
+        <Gallery />
+      </main>
+    </div>
+  );
+}
