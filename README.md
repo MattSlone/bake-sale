c72804c996802a83936721af3bdd618da9d73b9b
Matt Slone <slonem01@gmail.com>
2020-06-23 16:02:04 -0400

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Gobbelty Gook
+#SETUP
+
+1) run "npm install"
+2) run "npm build" (technically unnecessary but just do it for now)
+
+#START
+1) Run "node server/app" to start express server
+2) Run "npm start"
--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, '../build')));
 
 app.get('/test', function (req, res) {
- return res.send('Hello World!');
+ console.log("testing")
 });
 
 app.get('/', function (req, res) {
